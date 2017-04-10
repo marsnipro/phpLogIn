7cf6f5d42e529dd6b420e94634e6b9abd2aaaadd
marsnipro <marsnipro@gmail.com>
2017-04-10 17:18:16 +0900

    Update register.php

--- register.php
+++ register.php
@@ -5,10 +5,24 @@
     <body>
         <h2>Registration Page</h2>
         <a href="index.php">Click here to go back<br/><br/>
-        <form action="register.php" method="POST">
+        <form action="checklogin.php" method="POST">
            Enter Username: <input type="text" name="username" required="required" /> <br/>
            Enter password: <input type="password" name="password" required="required" /> <br/>
            <input type="submit" value="Register"/>
         </form>
     </body>
 </html>
+
+alert("Username has been taken!");</script>'; // Prompts the user
+           Print '<script>window.location.assign("register.php");</script>'; // redirects to register.php
+        }
+    }
+
+    if($bool)
+    {
+        mysql_query("INSERT INTO users (username, password) VALUES ('$username', 'password')"); // inserts value into table users
+        Print '<script>alert("Successfully Registered!");</script>'; // Prompts the user
+        Print '<script>window.location.assign("register.php");</script>'; // redirects to register.php
+    }
+}
+?>
